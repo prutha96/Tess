56770c8d4c8120c5a623a1118ea0c189a55d8c89
prutha96 <pruthadubhashi@gmail.com>
2016-07-24 17:16:02 +0530

    Master

--- app/src/main/java/com/example/user1/tess/MainActivity.java
+++ app/src/main/java/com/example/user1/tess/MainActivity.java
@@ -38,12 +38,9 @@ public class MainActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "com.example.user1.tess.MESSAGE";
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
-    static final int REQUEST_IMAGE_SELECT = 2;
 
     Button click;
-    Button select;
     ImageView imageView;
-    //TextView textView;
     String path;
     public static final String TAG = "PermissionTag";
 
@@ -58,69 +55,10 @@ public class MainActivity extends AppCompatActivity {
         imageView = (ImageView) findViewById(R.id.imageView);
     }
 
-    public boolean isStoragePermissionGranted() {
-        if (Build.VERSION.SDK_INT >= 23) {
-            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                    == PackageManager.PERMISSION_GRANTED) {
-                Log.v(TAG, "Permission is granted");
-                return true;
-            } else {
-
-                Log.v(TAG, "Permission is revoked");
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
-                return false;
-            }
-        } else { //permission is automatically granted on sdk<23 upon installation
-            Log.v(TAG, "Permission is granted");
-            return true;
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            Log.v(TAG, "Permission: " + permissions[0] + "was " + grantResults[0]);
-            //resume tasks needing this permission
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     protected void dispatchTakePictureIntent(View view1) {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null)
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-    }
-
-    protected void dispatchSelectPictureIntent(View view2) {
-        boolean StoragePermissionGranted = isStoragePermissionGranted();
-
-        if (StoragePermissionGranted) {
-            Intent selectPictureIntent = new Intent(Intent.ACTION_PICK,
-                    android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-            startActivityForResult(selectPictureIntent, REQUEST_IMAGE_SELECT);
-        }
     }
 
     @Override
@@ -128,7 +66,6 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
-            //imageView.setImageBitmap(imageBitmap);
 
             try {
                 path = saveImageToInternalStorage(imageBitmap);
@@ -139,19 +76,6 @@ public class MainActivity extends AppCompatActivity {
             loadImageFromStorage(path);
 
             performOCR(imageBitmap);
-        }else{
-           try {
-               Uri uri = data.getData();
-               System.out.println("uri:::" + uri.getPath());
-               Bitmap imageBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(),uri);
-               System.out.println("bitmap:::" + imageBitmap);
-               if (null != uri) {
-                   imageView.setImageURI(uri);
-               }
-               performOCR(imageBitmap);
-           }catch (Exception ex){
-               ex.printStackTrace();
-           }
         }
 
     }
@@ -188,7 +112,6 @@ public class MainActivity extends AppCompatActivity {
 
     void performOCR(Bitmap bitmap) {
         try {
-            boolean StoragePermissionGranted = isStoragePermissionGranted();
 
             TessBaseAPI baseAPI = new TessBaseAPI();
 
@@ -203,7 +126,6 @@ public class MainActivity extends AppCompatActivity {
             String recognisedText = baseAPI.getUTF8Text();
             baseAPI.end();
 
-            //textView.setText(recognisedText);
             Intent text = new Intent(this, DisplayText.class);
             text.putExtra(EXTRA_MESSAGE, recognisedText);
             startActivity(text);
